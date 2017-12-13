690191dba768536ab39226a2ecf4705834357974
Rafael Ferraro <ferraromv@gmail.com>
2017-12-12 22:39:30 -0200

    Adding OpenClosed principle example

---
+++ open_closed_principle.rb
@@ -0,0 +1,69 @@
+class Square
+  attr_reader :length
+
+  def initialize(length)
+    @length = length
+  end
+end
+
+class Circle
+  attr_reader :radius
+
+  def initialize(radius)
+    @radius = radius
+  end
+end
+
+class SumTotalArea
+  attr_reader :shape
+
+  def initialize(shape)
+    @shape = shape
+  end
+
+  def sum
+    if shape.is_a?(Square)
+      shape.length * 2
+    elsif shape.is_a?(Circle)
+      (shape.radius + 3) * 5
+    end
+  end
+end
+
+###
+
+class Square
+  attr_reader :length
+
+  def initialize(length)
+    @length = length
+  end
+
+  def calculate
+    @length * 2
+  end
+end
+
+class Circle
+  attr_reader :radius
+
+  def initialize(radius)
+    @radius = radius
+  end
+
+  def calculate
+    (@radius + 3) * 5
+  end
+end
+
+class SumTotalArea
+  attr_reader :shape
+
+  def initialize(shapes)
+    @shapes = shapes
+  end
+
+  def sum
+    shapes.map(&:calculate)
+  end
+end
